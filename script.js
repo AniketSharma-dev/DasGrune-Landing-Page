7ab8530bafd4ffd63e214ae89a7dfb0cc84a612d
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-10-05 19:11:19 +0530

    added files

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
+function menuToggle() {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
 
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-    menuToggle.classList.toggle('open');
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    menuToggle.classList.toggle("open");
+  });
+}
 
-});
+function mouse() {
+  var elem = document.querySelectorAll(".elem");
+  var dynamic = document.querySelector(".dynamic-image");
+  elem.forEach((e) => {
+    e.addEventListener("mouseover", function () {
+      dynamic.style.display = "block";
+      
+      var image = e.getAttribute("data-image")
+
+      dynamic.style.backgroundImage = `url(${image})`;
+    });
+    e.addEventListener("mouseout", function () {
+      dynamic.style.display = "none";
+    });
+  });
+}
+
+mouse();
+menuToggle();
